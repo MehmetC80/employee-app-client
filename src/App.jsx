8939d3f23bbb024e06b9d8c5127f1dd8201a9584
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-01-23 15:56:14 +0100

    add taiwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className=" text-center text-yellow-400">Employee App</h1>
+    </div>
+  );
 }
 
 export default App;
